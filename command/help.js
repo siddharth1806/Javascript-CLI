25ebacda6fb928876e96f8d48c9219d7059be7f5
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2021-07-25 21:55:44 +0530

    Create help.js

---
+++ command/help.js
@@ -0,0 +1,9 @@
+function tree()
+{
+    console.log("1. node main.js tree with path");
+    console.log("2. node main.js organize with path");
+    console.log("3. node main.js help");
+}
+module.exports={
+    fxn:tree
+}
